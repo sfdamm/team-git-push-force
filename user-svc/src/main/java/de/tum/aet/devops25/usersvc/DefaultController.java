8a181ef2c9af1aae513ce14642e84ffd1ee4ffe8
Claudiaaaaaaaaa <claudia.duenzinger@tum.de>
2025-05-22 18:05:18 +0200

    added Spring Boot Application for user-svc with Docker

---
+++ user-svc/src/main/java/de/tum/aet/devops25/usersvc/DefaultController.java
@@ -0,0 +1,13 @@
+package de.tum.aet.devops25.usersvc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, this is the default screen!";
+    }
+}
---
+++ user-svc/src/main/java/de/tum/aet/devops25/usersvc/UserSvc.java
@@ -0,0 +1,13 @@
+package de.tum.aet.devops25.usersvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSvc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSvc.class, args);
+	}
+
+}
