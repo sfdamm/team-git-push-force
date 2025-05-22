2b36c921723d1b7df6a6ab5cc93261c2d37a266c
Claudiaaaaaaaaa <claudia.duenzinger@tum.de>
2025-05-22 18:11:14 +0200

    added Spring Boot Application for concept-svc with Docker

---
+++ concept-svc/src/main/java/de/tum/aet/devops25/conceptsvc/ConceptSvc.java
@@ -0,0 +1,13 @@
+package de.tum.aet.devops25.conceptsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptSvc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptSvc.class, args);
+	}
+
+}
---
+++ concept-svc/src/test/java/de/tum/aet/devops25/conceptsvc/UserSvcTest.java
@@ -0,0 +1,13 @@
+package de.tum.aet.devops25.usersvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSvcTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
